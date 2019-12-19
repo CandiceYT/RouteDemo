75fa1b9584f914f01f098950e2f1681a524b2d09
candiceYT <m13152870721@163.com>
2019-12-19 19:08:35 +0800

    compile gaode sdk

--- app/src/main/java/com/example/routedemo/MainActivity.java
+++ app/src/main/java/com/example/routedemo/MainActivity.java
@@ -1,13 +1,304 @@
 package com.example.routedemo;
 
-import android.support.v7.app.AppCompatActivity;
+import android.Manifest;
+import android.annotation.TargetApi;
+import android.app.AlertDialog;
+import android.content.DialogInterface;
+import android.content.Intent;
+import android.content.pm.PackageManager;
+import android.net.Uri;
+import android.os.Build;
 import android.os.Bundle;
+import android.provider.Settings;
+import android.support.v7.app.AppCompatActivity;
+import android.widget.CheckBox;
+import android.widget.CompoundButton;
 
-public class MainActivity extends AppCompatActivity {
+import com.amap.api.maps.MapView;
+
+import java.lang.reflect.Method;
+import java.util.ArrayList;
+import java.util.List;
+
+public class MainActivity extends AppCompatActivity implements CompoundButton.OnCheckedChangeListener {
+    //躲避拥堵
+    private CheckBox cbAvoidCongestion;
+    //避免收费
+    private CheckBox cbSaveMoney;
+    //不走高速
+    private CheckBox cbNoHighway;
+    //高速优先
+    private CheckBox cbHighwayFirst;
+    private MapView map;
+    /**
+     * 判断是否需要检测，防止不停的弹框
+     */
+    private boolean isNeedCheck = true;
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        cbAvoidCongestion = findViewById(R.id.cb_avoid_congestion);
+        cbSaveMoney = findViewById(R.id.cb_save_money);
+        cbNoHighway = findViewById(R.id.cb_no_highway);
+        cbHighwayFirst = findViewById(R.id.cb_highway_first);
+        map = findViewById(R.id.map);
+        map.onCreate(savedInstanceState);
+        cbAvoidCongestion.setOnCheckedChangeListener(this);
+        cbSaveMoney.setOnCheckedChangeListener(this);
+        cbNoHighway.setOnCheckedChangeListener(this);
+        cbHighwayFirst.setOnCheckedChangeListener(this);
+    }
+
+
+    @Override
+    protected void onResume() {
+        super.onResume();
+        map.onResume();
+        checkPermission();
+    }
+
+
+    @Override
+    protected void onPause() {
+        super.onPause();
+        map.onPause();
+    }
+
+    @Override
+    protected void onStop() {
+        super.onStop();
+    }
+
+    @Override
+    protected void onSaveInstanceState(Bundle outState) {
+        super.onSaveInstanceState(outState);
+        map.onSaveInstanceState(outState);
+    }
+
+    @Override
+    protected void onDestroy() {
+        super.onDestroy();
+        map.onDestroy();
+    }
+
+
+//    ----------------------------权限检查--------------------------------
+
+    private void checkPermission() {
+        try {
+            super.onResume();
+            if (Build.VERSION.SDK_INT >= 23) {
+                if (isNeedCheck) {
+                    checkPermissions(needPermissions);
+                }
+            }
+        } catch (Throwable e) {
+            e.printStackTrace();
+        }
+    }
+
+
+    /**
+     * 需要进行检测的权限数组
+     */
+    protected String[] needPermissions = {
+            Manifest.permission.ACCESS_COARSE_LOCATION,
+            Manifest.permission.ACCESS_FINE_LOCATION,
+            Manifest.permission.WRITE_EXTERNAL_STORAGE,
+            Manifest.permission.READ_EXTERNAL_STORAGE,
+            Manifest.permission.READ_PHONE_STATE
+    };
+
+    private static final int PERMISSON_REQUESTCODE = 0;
+
+    /**
+     * @param
+     * @since 2.5.0
+     */
+    @TargetApi(23)
+    private void checkPermissions(String... permissions) {
+        try {
+            if (Build.VERSION.SDK_INT >= 23 && getApplicationInfo().targetSdkVersion >= 23) {
+                List<String> needRequestPermissonList = findDeniedPermissions(permissions);
+                if (null != needRequestPermissonList
+                        && needRequestPermissonList.size() > 0) {
+                    try {
+                        String[] array =
+                                needRequestPermissonList.toArray(new String[needRequestPermissonList.size()]);
+                        Method method = getClass().getMethod("requestPermissions",
+                                new Class[]{String[].class, int.class});
+                        method.invoke(this, array, 0);
+                    } catch (Throwable e) {
+
+                    }
+                }
+            }
+
+        } catch (Throwable e) {
+            e.printStackTrace();
+        }
+    }
+
+    /**
+     * 获取权限集中需要申请权限的列表
+     *
+     * @param permissions
+     * @return
+     * @since 2.5.0
+     */
+    @TargetApi(23)
+    private List<String> findDeniedPermissions(String[] permissions) {
+        try {
+            List<String> needRequestPermissonList = new ArrayList<String>();
+            if (Build.VERSION.SDK_INT >= 23 && getApplicationInfo().targetSdkVersion >= 23) {
+                for (String perm : permissions) {
+                    if (checkMySelfPermission(perm) != PackageManager.PERMISSION_GRANTED
+                            || shouldShowMyRequestPermissionRationale(perm)) {
+                        needRequestPermissonList.add(perm);
+                    }
+                }
+            }
+            return needRequestPermissonList;
+        } catch (Throwable e) {
+            e.printStackTrace();
+        }
+        return null;
+    }
+
+    private int checkMySelfPermission(String perm) {
+        try {
+            Method method = getClass().getMethod("checkSelfPermission", new Class[]{String.class});
+            Integer permissionInt = (Integer) method.invoke(this, perm);
+            return permissionInt;
+        } catch (Throwable e) {
+        }
+        return -1;
+    }
+
+    private boolean shouldShowMyRequestPermissionRationale(String perm) {
+        try {
+            Method method = getClass().getMethod("shouldShowRequestPermissionRationale",
+                    new Class[]{String.class});
+            Boolean permissionInt = (Boolean) method.invoke(this, perm);
+            return permissionInt;
+        } catch (Throwable e) {
+        }
+        return false;
+    }
+
+    /**
+     * 检测是否说有的权限都已经授权
+     *
+     * @param grantResults
+     * @return
+     * @since 2.5.0
+     */
+    private boolean verifyPermissions(int[] grantResults) {
+        try {
+            for (int result : grantResults) {
+                if (result != PackageManager.PERMISSION_GRANTED) {
+                    return false;
+                }
+            }
+        } catch (Throwable e) {
+            e.printStackTrace();
+        }
+        return true;
+    }
+
+    @TargetApi(23)
+    public void onRequestPermissionsResult(int requestCode,
+                                           String[] permissions, int[] paramArrayOfInt) {
+        try {
+            if (Build.VERSION.SDK_INT >= 23) {
+                if (requestCode == PERMISSON_REQUESTCODE) {
+                    if (!verifyPermissions(paramArrayOfInt)) {
+                        showMissingPermissionDialog();
+                        isNeedCheck = false;
+                    }
+                }
+            }
+        } catch (Throwable e) {
+            e.printStackTrace();
+        }
+    }
+
+    /**
+     * 显示提示信息
+     *
+     * @since 2.5.0
+     */
+    private void showMissingPermissionDialog() {
+        try {
+            AlertDialog.Builder builder = new AlertDialog.Builder(this);
+            builder.setTitle("提示");
+            builder.setMessage("当前应用缺少必要权限。\\n\\n请点击\\\"设置\\\"-\\\"权限\\\"-打开所需权限");
+
+            // 拒绝, 退出应用
+            builder.setNegativeButton("取消",
+                    new DialogInterface.OnClickListener() {
+                        @Override
+                        public void onClick(DialogInterface dialog, int which) {
+                            try {
+                                finish();
+                            } catch (Throwable e) {
+                                e.printStackTrace();
+                            }
+                        }
+                    });
+
+            builder.setPositiveButton("设置",
+                    new DialogInterface.OnClickListener() {
+                        @Override
+                        public void onClick(DialogInterface dialog, int which) {
+                            try {
+                                startAppSettings();
+                            } catch (Throwable e) {
+                                e.printStackTrace();
+                            }
+                        }
+                    });
+
+            builder.setCancelable(false);
+
+            builder.show();
+        } catch (Throwable e) {
+            e.printStackTrace();
+        }
+    }
+
+    /**
+     * 启动应用的设置
+     *
+     * @since 2.5.0
+     */
+    private void startAppSettings() {
+        try {
+            Intent intent = new Intent(
+                    Settings.ACTION_APPLICATION_DETAILS_SETTINGS);
+            intent.setData(Uri.parse("package:" + getPackageName()));
+            startActivity(intent);
+        } catch (Throwable e) {
+            e.printStackTrace();
+        }
+    }
+
+    @Override
+    public void onCheckedChanged(CompoundButton buttonView, boolean isChecked) {
+        switch (buttonView.getId()) {
+            case R.id.cb_avoid_congestion:
+                break;
+            case R.id.cb_highway_first:
+                break;
+            case R.id.cb_no_highway:
+                break;
+            case R.id.cb_save_money:
+                break;
+            default:
+                break;
+        }
     }
 }
